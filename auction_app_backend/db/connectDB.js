adde7428a1a6f818db8e994707086fbcf3dd2fa6
chatla-tarun-kumar123 <chatla.tarunkumar123@gmail.com>
2025-03-09 20:22:13 +0530

    removed mogodb url

--- auction_app_backend/db/connectDB.js
+++ auction_app_backend/db/connectDB.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(
-            "mongodb+srv://chatlatarunkumar123:pxQcM3E5Uj6QJQ6d@cluster0.q77u1.mongodb.net/auction_app"
-        );
+        await mongoose.connect();
     } catch (e) {
         console.error("MongoDB Connection Failed:", e);
         process.exit(1);
